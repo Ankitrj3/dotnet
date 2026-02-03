e6afa322404f6def65391ea2b0574053008de2cd
ankitrj3 <ankitrobinranjan@gmail.com>
2026-02-03 15:48:10 +0530

    Time Conversion

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static string TimeConversion(int Time)
+    {
+        int Hour = Time / 60;
+        int Min = Time % 60;
+        return $"{Hour}:{Min:F2}";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(TimeConversion(134));
+    }
+}
