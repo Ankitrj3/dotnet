6f6a2e6ed9caf23fca831518f554473393b43200
ankitrj3 <ankitrobinranjan@gmail.com>
2026-02-03 15:37:19 +0530

    MidPointRounding

---
+++ MidpointRounding/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static double AreaOfCircle(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area, 2, MidpointRounding.AwayFromZero);
+    }
+    public static void Main()
+    {
+        double r = 2.4;
+        Console.WriteLine(AreaOfCircle(r));
+    }
+}
