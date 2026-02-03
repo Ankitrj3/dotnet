5b7194e4cd291faa8154e2899303f3841b5686d3
ankitrj3 <ankitrobinranjan@gmail.com>
2026-02-03 15:13:54 +0530

    Nunit and Attributes

---
+++ Attributes/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Attributes
+{
+    [Obsolete("Use the Add(int,int) method instead")]
+    public void OldAdd(int a, int b)
+    {
+        Console.WriteLine(a+b);
+    }
+    public void Add(int x,int y)
+    {
+        Console.WriteLine(x+y);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Attributes a = new Attributes();
+        a.OldAdd(12,34);
+    }
+}
